d0ea0e6746b0ea2c39fb3a010ebba14600e156bf
sumitchaudhari <sumitchaudhari013@gmail.com>
2021-09-02 20:07:58 +0530

    array operation in js

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
-console.log('Hey this is my first piece of JS code!!! YAY')
+// console.log('Hey this is my first piece of JS code!!! YAY')
 
-console.log(12490239043902)
+// console.log(12490239043902)
 
-console.log({ name: "Yutika" })
+// console.log({ name: "sumit" })
 
-console.log(true)
-console.log(false)
+// console.log(true)
+// console.log(false)
 
-console.log(false, 234243, 'string', { name: "Alfred" })
+// console.log(false, 234243, 'string', { name: "Alfred" })
 
-console.warn("This is a warning")
+// console.warn("This is a warning")
 
-console.error("this is an error")
+// console.error("this is an error")
+
+        //   ---Data Types--
+        //      1)primitive ->Don't have any object and method associated with it ->int,string
+        //     
+        //      2)non-primitive
+ 
+        //   --Primitive DataType
+      
+        var Name="piyush";
+        Name = "sumit"
+        console.log(Name)
+
+        let age= 19
+        age=20
+        console.log(age)
+        console.log("Datatype of age is" ,typeof age)
+
+
+        let brand
+        console.log(brand)
+
+
+        // Non-Premitive DataType
+         
+        let arr=[1,2,3,4,5]
+        console.log(typeof arr)
+        arr.push("value")
+        console.log("Now array is :", arr)
+ 
+        
