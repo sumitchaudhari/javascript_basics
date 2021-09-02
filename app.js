39ee05f1766feb3e60a60e264fccf8ec5115f144
sumitchaudhari <sumitchaudhari013@gmail.com>
2021-09-02 19:26:58 +0530

    used console operations

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('hey this is my first piece of js code!!')
+
+console.log(1286776868)
+console.log({ name: "Sumit" })
+
+console.warn("This is a warning")
+
+console.error("This is an error")
